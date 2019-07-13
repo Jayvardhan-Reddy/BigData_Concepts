cd7f6f7de947de5e94b6becf613f8f90decbf560
Jayvardhan Reddy Vanchireddy <33086172+Jayvardhan-Reddy@users.noreply.github.com>
2019-07-13 07:53:36 -0400

    Inserting a new record

---
+++ Sqoop-Concepts/Sqoop-Incremental-Load/record_insertion_into_database.sql
@@ -0,0 +1,4 @@
+-- 1. Login to mysql in your desired distribution(CDH | HDP | MapR)
+-- 2. Navigate to Customers table
+
+insert into customers values(12436,'Jay','Reddy','****@gmail.com','hello','R T Nagar','Bangalore','Karnataka','500032');
